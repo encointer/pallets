22b104ad5e16adb912f60ef39242fe7a5f97ce69
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2020-11-17 08:12:13 +0100

    addition of bazaar pallet. Not yet tested

---
+++ bazaar/src/lib.rs
@@ -0,0 +1,132 @@
+// Copyright (c) 2019 Alain Brenzikofer
+// This file is part of Encointer
+//
+// Encointer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Encointer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+//! # Encointer Bazaar Module
+//!
+//! provides functionality for
+//! - creating new bazaar entries
+//! - removing existing entries
+//!
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::{
+    decl_event, decl_module, decl_storage,
+    dispatch::DispatchResult,
+    ensure,
+    //storage::StorageValue,
+    //traits::Get,
+    weights::{DispatchClass, Pays},
+    debug
+    //StorageMap nötig?
+};
+use sp_core::RuntimeDebug;
+use frame_system::ensure_signed;
+use sp_timestamp::OnTimestampSet;
+//use rstd::prelude::*;
+use codec::{Decode, Encode};
+//use sp_runtime::traits::{Saturating, CheckedAdd, CheckedDiv, Zero};
+//use rstd::ops::Rem;
+//use sp_std::vec::Vec;
+
+pub trait Trait: frame_system::Trait  + timestamp::Trait
+{
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+}
+
+// Logger target
+const LOG: &str = "encointer";
+
+// The pallet's runtime storage items.
+// https://substrate.dev/docs/en/knowledgebase/runtime/storage
+decl_storage! {
+    trait Store for Module<T: Trait> as TemplateModule {
+        /// The storage item for our proofs.
+        /// It maps a proof to the user who made the claim and when they made it.
+        Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
+    }
+}
+}
+// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+// These functions materialize as "extrinsics", which are often compared to transactions.
+// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // initialisation
+        type Error = Error<T>;
+        fn deposit_event() = default;
+
+        /// Allow a user to claim ownership of an unclaimed proof.
+        #[weight = 10_000]
+        fn create_claim(origin, proof: Vec<u8>) {
+            // Check that the extrinsic was signed and get the signer.
+            // This function will return an error if the extrinsic is not signed.
+            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
+            let sender = ensure_signed(origin)?;
+
+            // Verify that the specified proof has not already been claimed.
+            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+
+            // Get the block number from the FRAME System module.
+            let current_block = <frame_system::Module<T>>::block_number();
+
+            // Store the proof with the sender and block number.
+            Proofs::<T>::insert(&proof, (&sender, current_block));
+
+            // Emit an event that the claim was created.
+            Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
+        }
+
+        /// Allow the owner to revoke their claim.
+        #[weight = 10_000]
+        fn revoke_claim(origin, proof: Vec<u8>) {
+            // Check that the extrinsic was signed and get the signer.
+            // This function will return an error if the extrinsic is not signed.
+            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
+            let sender = ensure_signed(origin)?;
+
+            // Verify that the specified proof has been claimed.
+            ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+            // Get owner of the claim.
+            let (owner, _) = Proofs::<T>::get(&proof);
+
+            // Verify that sender of the current call is the claim owner.
+            ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+            // Remove claim from storage.
+            Proofs::<T>::remove(&proof);
+
+            // Emit an event that the claim was erased.
+            Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
+        }
+    }
+}
+
+decl_event! {
+    pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+        /// Event emitted when a shop is uploaded. [who, shop]
+        ShopCreated(AccountId, Vec<u8>),
+        /// Event emitted when a shop is revoked by the owner. [who, shop]
+        ShopRevoked(AccountId, Vec<u8>),
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests;
---
+++ bazaar/src/tests.rs
@@ -0,0 +1,525 @@
+// Copyright (c) 2019 Alain Brenzikofer
+// This file is part of Encointer
+//
+// Encointer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Encointer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+use crate::{Module, Trait, CeremonyPhaseType, GenesisConfig};
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+	Perbill,
+};
+use frame_support::traits::UnfilteredDispatchable;
+use sp_core::H256;
+use frame_support::{assert_ok, impl_outer_origin, impl_outer_event, parameter_types,
+    traits::{OnFinalize, OnInitialize}};
+use runtime_io::TestExternalities;
+use inherents::ProvideInherent;
+use std::ops::Rem;
+
+impl_outer_origin! {
+	pub enum Origin for TestRuntime {}
+}
+
+mod simple_event {
+	pub use crate::Event;
+}
+
+impl_outer_event! {
+	pub enum TestEvent for TestRuntime {
+		simple_event,
+		frame_system<T>,
+	}
+}
+
+parameter_types! {
+	pub const MomentsPerDay: u64 = 86_400_000; // [ms/d]
+}
+impl Trait for TestRuntime {
+    type Event = TestEvent;
+    type OnCeremonyPhaseChange = ();
+    type MomentsPerDay = MomentsPerDay;
+}
+
+type AccountId = u64;
+type Moment = u64;
+
+// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct TestRuntime;
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: u32 = 1024;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+}
+impl frame_system::Trait for TestRuntime {
+    type BaseCallFilter = ();    
+	type Origin = Origin;
+	type Index = u64;
+	type Call = ();
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = TestEvent;
+	type BlockHashCount = BlockHashCount;
+    type MaximumBlockWeight = MaximumBlockWeight;
+	type DbWeight = ();
+	type BlockExecutionWeight = ();
+	type ExtrinsicBaseWeight = ();    
+    type MaximumBlockLength = MaximumBlockLength;
+    type MaximumExtrinsicWeight = MaximumBlockWeight;
+	type AvailableBlockRatio = AvailableBlockRatio;
+    type Version = ();
+	type AccountData = ();
+	type OnNewAccount = ();
+    type OnKilledAccount = ();   
+    type SystemWeightInfo = (); 
+    type PalletInfo = ();
+}
+
+parameter_types! {
+    pub const MinimumPeriod: u64 = 1;
+}
+impl timestamp::Trait for TestRuntime {
+	type Moment = Moment;
+	type OnTimestampSet = EncointerScheduler;
+    type MinimumPeriod = MinimumPeriod;
+    type WeightInfo = ();
+}
+
+pub struct ExtBuilder{
+    equal_phase_duration: Moment,
+}
+
+impl Default for ExtBuilder {
+    fn default() -> Self {
+        Self {
+            equal_phase_duration: 86_400_000,
+        }
+    }
+}
+
+const MASTER: AccountId = 0;
+
+impl ExtBuilder {
+    pub fn equal_phase_duration(mut self, d: Moment) -> Self {
+        self.equal_phase_duration = d;
+        self
+    }
+
+    pub fn build(&self) -> TestExternalities {
+        let mut storage = frame_system::GenesisConfig::default()
+            .build_storage::<TestRuntime>()
+            .unwrap();
+        GenesisConfig::<TestRuntime> {
+            current_phase: CeremonyPhaseType::REGISTERING,
+            current_ceremony_index: 1,
+            ceremony_master: MASTER,
+            phase_durations: vec![
+                (CeremonyPhaseType::REGISTERING, self.equal_phase_duration),
+                (CeremonyPhaseType::ASSIGNING, self.equal_phase_duration),
+                (CeremonyPhaseType::ATTESTING, self.equal_phase_duration),
+            ]
+        }
+        .assimilate_storage(&mut storage)
+        .unwrap();
+        runtime_io::TestExternalities::from(storage)
+    }
+}
+pub type System = frame_system::Module<TestRuntime>;
+pub type Timestamp = timestamp::Module<TestRuntime>;
+pub type EncointerScheduler = Module<TestRuntime>;
+
+/// Run until a particular block.
+pub fn run_to_block(n: u64) {
+	while System::block_number() < n {
+		if System::block_number() > 1 {
+            System::on_finalize(System::block_number());
+        }
+        Timestamp::on_finalize(System::block_number());
+		System::set_block_number(System::block_number() + 1);
+		System::on_initialize(System::block_number());
+	}
+}
+
+pub fn set_timestamp(t: u64) {
+    let _ = <timestamp::Module<TestRuntime> as ProvideInherent>::Call::set(t)
+        .dispatch_bypass_filter(Origin::none());    
+}
+
+#[test]
+fn ceremony_phase_statemachine_works() {
+    ExtBuilder::default().build().execute_with(|| {
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ASSIGNING
+        );
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ATTESTING
+        );
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 2);
+    });
+}
+
+#[test]
+fn timestamp_callback_works() {
+    ExtBuilder::default().build().execute_with(|| {
+        //large offset since 1970 to when first block is generated
+        const GENESIS_TIME: u64 = 1_585_058_843_000;
+        const ONE_DAY: u64 = 86_400_000;
+        System::set_block_number(0);
+        
+        set_timestamp(GENESIS_TIME);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (GENESIS_TIME - GENESIS_TIME.rem(ONE_DAY)) + ONE_DAY);
+
+        run_to_block(1);
+        set_timestamp(GENESIS_TIME + ONE_DAY);
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ASSIGNING
+        );
+
+        run_to_block(2);
+        set_timestamp(GENESIS_TIME + 2 * ONE_DAY);
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ATTESTING
+        );
+
+        run_to_block(3);
+        set_timestamp(GENESIS_TIME + 3 * ONE_DAY);
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 2);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+
+    });
+}
+
+const TEN_MIN: u64 = 600_000;
+const ONE_DAY: u64 = 86_400_000;
+
+#[test]
+fn push_one_day_works() {
+    ExtBuilder::default().equal_phase_duration(ONE_DAY).build().execute_with(|| {
+        let genesis_time: u64 = 0 * TEN_MIN + 1;
+
+        System::set_block_number(0);
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 1 * ONE_DAY);
+
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );   
+
+        run_to_block(1);
+        set_timestamp(genesis_time + TEN_MIN);
+
+        assert_ok!(EncointerScheduler::push_by_one_day(Origin::signed(
+            MASTER
+        )));  
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 2 * ONE_DAY);
+
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );   
+    
+    });
+}
+#[test]
+fn resync_catches_up_short_cycle_times_at_genesis_during_first_registering_phase() {
+    ExtBuilder::default().equal_phase_duration(TEN_MIN).build().execute_with(|| {
+        // CASE1: genesis happens during first REGISTERING phase of the day
+        let genesis_time: u64 = 0 * TEN_MIN + 1;
+
+        System::set_block_number(0);
+        
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 1 * TEN_MIN);
+    });
+}
+    
+#[test]
+fn resync_catches_up_short_cycle_times_at_genesis_during_third_registering_phase() {
+    ExtBuilder::default().equal_phase_duration(TEN_MIN).build().execute_with(|| {
+        // CASE2: genesis happens during 3rd REGISTERING phase of the day
+        let genesis_time: u64 = 6 * TEN_MIN + 1;
+
+        System::set_block_number(0);
+        
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 7 * TEN_MIN);
+    });
+}
+    
+#[test]
+fn resync_catches_up_short_cycle_times_at_genesis_during_third_assigning_phase() {
+    ExtBuilder::default().equal_phase_duration(TEN_MIN).build().execute_with(|| {
+        // CASE3: genesis happens during 3rd ASSIGNING phase of the day
+        let genesis_time: u64 = 7 * TEN_MIN + 1;
+
+        System::set_block_number(0);
+        
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 10 * TEN_MIN);
+    });
+}
+    
+#[test]
+fn resync_catches_up_short_cycle_times_at_genesis_during_third_attesting_phase() {
+    ExtBuilder::default().equal_phase_duration(TEN_MIN).build().execute_with(|| {
+        // CASE4: genesis happens during 3rd ATTESTING phase of the day
+        let genesis_time: u64 = 8 * TEN_MIN + 1;
+
+        System::set_block_number(0);
+        
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 10 * TEN_MIN);
+
+    });
+}
+
+#[test]
+fn resync_after_next_phase_works() {
+    ExtBuilder::default().equal_phase_duration(ONE_DAY).build().execute_with(|| {
+        let genesis_time: u64 = 0;
+        
+        System::set_block_number(0);
+        
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 1 * ONE_DAY);
+
+        run_to_block(1);
+        set_timestamp(genesis_time + TEN_MIN);  
+
+        // now use next_phase manually 
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ASSIGNING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 2 * ONE_DAY);
+        // this means that we merely anticipated the ASSIGNING_PHASE. NExt ATTESTING will still start as if next_phase() had not been called 
+
+        run_to_block(2);
+        set_timestamp(genesis_time + 2*TEN_MIN);              
+
+        // again
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ATTESTING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 3 * ONE_DAY);
+        // this means that we merely anticipated the ATTESTING phase. NExt REGISTERING will still start as if next_phase() had not been called 
+    
+        run_to_block(3);
+        set_timestamp(genesis_time + 3*TEN_MIN);              
+
+        // again
+        // because we would skip an entire Cycle now, we resync to the next 
+        // even next_phase_timestamp in the future
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 2);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 1 * ONE_DAY);
+        // now the next ASSIGNING phase starts exactly at the time it would have startet if next_phase had not been called. 
+        // But the ceremony index increased by one
+
+    });
+}
+
+#[test]
+fn resync_after_next_phase_works_during_assigning() {
+    ExtBuilder::default().equal_phase_duration(ONE_DAY).build().execute_with(|| {
+        let genesis_time: u64 = 0;
+        
+        System::set_block_number(0);
+        
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 1 * ONE_DAY);
+
+        run_to_block(1);
+        set_timestamp(genesis_time + ONE_DAY+ TEN_MIN);  
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ASSIGNING
+        );
+
+        // now use next_phase manually 
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 2);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ASSIGNING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 2 * ONE_DAY);
+
+    });
+}
+#[test]
+fn resync_after_next_phase_works_during_attesting() {
+    ExtBuilder::default().equal_phase_duration(ONE_DAY).build().execute_with(|| {
+        let genesis_time: u64 = 0;
+        
+        System::set_block_number(0);
+        
+        set_timestamp(genesis_time);
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 1);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::REGISTERING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 1 * ONE_DAY);
+
+        run_to_block(1);
+        set_timestamp(genesis_time + 1*ONE_DAY+ TEN_MIN);  
+
+        run_to_block(2);
+        set_timestamp(genesis_time + 2*ONE_DAY+ TEN_MIN);  
+        
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ATTESTING
+        );
+
+        // now use next_phase manually 
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+        assert_ok!(EncointerScheduler::next_phase(Origin::signed(
+            MASTER
+        )));
+
+        assert_eq!(EncointerScheduler::current_ceremony_index(), 2);
+        assert_eq!(
+            EncointerScheduler::current_phase(),
+            CeremonyPhaseType::ATTESTING
+        );
+        assert_eq!(EncointerScheduler::next_phase_timestamp(), 
+            (genesis_time - genesis_time.rem(ONE_DAY)) + 3 * ONE_DAY);
+
+    });
+}
