b186410d82a1177be21bb19f4d295e535860f9f6
BignaH <73821294+BignaH@users.noreply.github.com>
2020-11-25 14:23:14 +0100

    Bazaar pallet (#10)
    * add bazaar pallet. 
    
    Co-authored-by: Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>

---
+++ bazaar/src/lib.rs
@@ -0,0 +1,165 @@
+// Copyright (c) 2019 Alain Brenzikofer
+// This file is part of Encointer
+//
+// Encointer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Encointer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+//! # Encointer Bazaar Module
+//!
+//! provides functionality for
+//! - creating new bazaar entries (shop and articles)
+//! - removing existing entries (shop and articles)
+//!
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(test)]
+#[macro_use]
+extern crate approx;
+
+use frame_support::{
+    decl_event, decl_module, decl_storage, decl_error,
+    dispatch::DispatchResult,
+    ensure,
+    storage::{StorageDoubleMap, StorageMap},
+    weights::{DispatchClass, Pays},
+    debug
+};
+use rstd::prelude::*;
+use frame_system::ensure_signed;
+use codec::{Decode, Encode};
+
+use encointer_currencies::{CurrencyIdentifier};
+
+pub trait Trait: frame_system::Trait 
+    + encointer_currencies::Trait 
+{
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+}
+
+pub type ShopIdentifier = u64; 
+pub type ArticleIdentifier = u64; 
+
+decl_storage! {
+    trait Store for Module<T: Trait> as Bazaar {
+        // Maps the shop or article owner to the respective items 
+        pub ShopsOwned get(fn shops_owned): double_map hasher(blake2_128_concat) CurrencyIdentifier, hasher(blake2_128_concat) T::AccountId => Vec<ShopIdentifier>;
+        pub ArticlesOwned get(fn articles_owned): double_map hasher(blake2_128_concat) CurrencyIdentifier, hasher(blake2_128_concat) T::AccountId => Vec<ArticleIdentifier>; 
+        // Item owner
+        pub ShopOwner get(fn shop_owner): double_map hasher(blake2_128_concat) CurrencyIdentifier, hasher(blake2_128_concat) ShopIdentifier => T::AccountId;
+        pub ArticleOwner get(fn article_owner): double_map hasher(blake2_128_concat) CurrencyIdentifier, hasher(blake2_128_concat) ArticleIdentifier => (T::AccountId, ShopIdentifier);
+        // The set of all shops and articles per currency (community)
+        pub ShopRegistry get(fn shop_registry): map hasher(blake2_128_concat) CurrencyIdentifier => Vec<ShopIdentifier>;
+        pub ArticleRegistry get(fn article_registry): map hasher(blake2_128_concat) CurrencyIdentifier => Vec<ArticleIdentifier>;
+    }
+}
+
+decl_event! {
+    pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+        /// Event emitted when a shop is uploaded. [currency, who, shop]
+        ShopCreated(CurrencyIdentifier, AccountId, ShopIdentifier),
+        /// Event emitted when a shop is removed by the owner. [currency, who, shop]
+        ShopRemoved(CurrencyIdentifier, AccountId, ShopIdentifier),
+    }
+}
+
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		/// no such shop exisiting that could be deleted
+        NoSuchShop,
+        /// shop can not be created twice
+        ShopAlreadyCreated,
+        /// shop can not be removed by anyone else than its owner
+        OnlyOwnerCanRemoveShop,
+	}
+}
+
+// TODO: Check if URL valid
+// TODO: Add Article Upload / Removal
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // initialisation
+        fn deposit_event() = default;
+
+        /// Allow a user to create a shop
+        #[weight = 10_000]
+        pub fn upload_shop(origin, cid: CurrencyIdentifier, shop: ShopIdentifier) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer
+            let sender = ensure_signed(origin)?;
+            // Check that the supplied currency is actually registered
+            ensure!(<encointer_currencies::Module<T>>::currency_identifiers().contains(&cid),
+                "CurrencyIdentifier not found");
+
+            let mut owned_shops = ShopsOwned::<T>::get(cid, &sender);
+            let mut shops = ShopRegistry::get(cid);
+
+            // Verify that the specified shop has not already been created with fast search
+            ensure!(!ShopOwner::<T>::contains_key(cid, &shop), Error::<T>::ShopAlreadyCreated);   
+            
+            // Add the shop to the registries
+            owned_shops.push(shop.clone());
+            shops.push(shop.clone());
+            // Update blockchain 
+            ShopsOwned::<T>::insert(cid, &sender, owned_shops);
+            ShopOwner::<T>::insert(cid, &shop, &sender);
+            ShopRegistry::insert(cid, shops);  
+            // Emit an event that the shop was created
+            Self::deposit_event(RawEvent::ShopCreated(cid, sender, shop));
+            Ok(())                     
+        }
+
+        /// Allow a user to remove their shop
+        #[weight = 10_000]
+        pub fn remove_shop(origin, cid:CurrencyIdentifier, shop: ShopIdentifier) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer
+            let sender = ensure_signed(origin)?;
+
+            let mut owned_shops = ShopsOwned::<T>::get(cid, &sender);
+            let mut shops = ShopRegistry::get(cid);
+
+            // Verify that the removal request is coming from the righteous owner
+            let shop_owner = ShopOwner::<T>::get(cid, &shop);
+            ensure!(shop_owner == sender, Error::<T>::OnlyOwnerCanRemoveShop);
+
+            // Get the index of the shop in the owner list
+            match owned_shops.binary_search(&shop) {
+                // Get the index of the shop registry
+                Ok(shop_registry_index) => {
+                    match owned_shops.binary_search(&shop) {
+                        // If the search succeeds, delete the respective entries
+                        Ok(onwed_shops_index) => {
+                            // Remove the shop from the local registries
+                            owned_shops.remove(onwed_shops_index);
+                            shops.remove(shop_registry_index);
+                            // Update blockchain
+                            ShopsOwned::<T>::insert(cid, &sender, owned_shops);    
+                            ShopRegistry::insert(cid, shops);                             
+                            ShopOwner::<T>::remove(cid, &shop);
+                            // Emit an event that the shop was removed
+                            Self::deposit_event(RawEvent::ShopRemoved(cid, sender, shop));    
+                            Ok(())       
+                        },
+                        // If the search fails, no such shop is owned
+                        Err(_) => Err(Error::<T>::NoSuchShop.into()),
+                    }      
+                },
+                // If the search fails, no such shop is owned
+                Err(_) => Err(Error::<T>::NoSuchShop.into()),       
+            }                   
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests;
---
+++ bazaar/src/tests.rs
@@ -0,0 +1,360 @@
+// Copyright (c) 2019 Alain Brenzikofer
+// This file is part of Encointer
+//
+// Encointer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Encointer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+//! Unit tests for the tokens module.
+
+use super::*;
+//use crate::{GenesisConfig, Module, Trait};
+use crate::{Module, Trait};
+use encointer_currencies::{CurrencyIdentifier, Location, Degree};
+use externalities::set_and_run_with_externalities;
+use sp_core::{hashing::blake2_256, sr25519, Blake2Hasher, Pair, Public, H256};
+use sp_runtime::traits::{CheckedAdd, IdentifyAccount, Member, Verify};
+use sp_runtime::{
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
+    Perbill,
+};
+use std::{cell::RefCell, collections::HashSet};
+use frame_support::traits::{Currency, FindAuthor, Get, LockIdentifier};
+use frame_support::{assert_ok, impl_outer_event, impl_outer_origin, parameter_types};
+use sp_keyring::AccountKeyring;
+
+use fixed::traits::LossyFrom;
+use fixed::types::{I32F32, I9F23, I9F55};
+
+/// The signature type used by accounts/transactions.
+pub type Signature = sr25519::Signature;
+/// An identifier for an account on this system.
+pub type AccountId = <Signature as Verify>::Signer;
+
+
+thread_local! {
+    static EXISTENTIAL_DEPOSIT: RefCell<u64> = RefCell::new(2);
+}
+pub type BlockNumber = u64;
+pub type Balance = u64;
+
+pub struct ExistentialDeposit;
+impl Get<u64> for ExistentialDeposit {
+    fn get() -> u64 {
+        EXISTENTIAL_DEPOSIT.with(|v| *v.borrow())
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct TestRuntime;
+
+impl Trait for TestRuntime {
+    type Event = ();
+}
+
+impl encointer_balances::Trait for TestRuntime {
+	type Event = ();
+}
+
+pub type EncointerBazaar = Module<TestRuntime>;
+
+impl encointer_currencies::Trait for TestRuntime {
+    type Event = ();
+}
+pub type EncointerCurrencies = encointer_currencies::Module<TestRuntime>;
+
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub const MaximumBlockWeight: u32 = 1024;
+    pub const MaximumBlockLength: u32 = 2 * 1024;
+    pub const AvailableBlockRatio: Perbill = Perbill::one();
+}
+impl frame_system::Trait for TestRuntime {
+    type BaseCallFilter = ();       
+    type Origin = Origin;
+    type Index = u64;
+    type Call = ();
+    type BlockNumber = BlockNumber;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = ();
+    type BlockHashCount = BlockHashCount;
+    type MaximumBlockWeight = MaximumBlockWeight;
+	type DbWeight = ();
+	type BlockExecutionWeight = ();
+	type ExtrinsicBaseWeight = ();    
+    type MaximumBlockLength = MaximumBlockLength;
+    type MaximumExtrinsicWeight = MaximumBlockWeight;    
+    type AvailableBlockRatio = AvailableBlockRatio;
+    type Version = ();
+    type AccountData = balances::AccountData<u64>;
+	type OnNewAccount = ();
+    type OnKilledAccount = ();  
+    type SystemWeightInfo = (); 
+    type PalletInfo = ();     
+}
+
+pub type System = frame_system::Module<TestRuntime>;
+
+parameter_types! {
+    pub const TransferFee: Balance = 0;
+    pub const CreationFee: Balance = 0;
+    pub const TransactionBaseFee: u64 = 0;
+    pub const TransactionByteFee: u64 = 0;
+}
+
+impl balances::Trait for TestRuntime {
+    type Balance = Balance;
+    type Event = ();
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type WeightInfo = ();
+    type MaxLocks = ();   
+}
+
+pub struct ExtBuilder;
+
+impl ExtBuilder {
+    pub fn build() -> runtime_io::TestExternalities {
+        let storage = frame_system::GenesisConfig::default()
+            .build_storage::<TestRuntime>()
+            .unwrap();
+        runtime_io::TestExternalities::from(storage)
+    }
+}
+
+impl_outer_origin! {
+    pub enum Origin for TestRuntime {}
+}
+
+/// register a simple test currency with 3 meetup locations and well known bootstrappers
+fn register_test_currency() -> CurrencyIdentifier {
+    // all well-known keys are boottrappers for easy testen afterwards
+    let alice = AccountId::from(AccountKeyring::Alice);
+    let bob = AccountId::from(AccountKeyring::Bob);
+    let charlie = AccountId::from(AccountKeyring::Charlie);
+    
+    let a = Location::default(); // 0, 0
+    
+    let b = Location {
+        lat: Degree::from_num(1),
+        lon: Degree::from_num(1),
+    };
+    let c = Location {
+        lat: Degree::from_num(2),
+        lon: Degree::from_num(2),
+    };
+    let loc = vec![a, b, c];
+    let bs = vec![
+        alice.clone(),
+        bob.clone(),
+        charlie.clone(),
+    ];
+    assert_ok!(EncointerCurrencies::new_currency(
+        Origin::signed(alice.clone()),
+        loc.clone(),
+        bs.clone()
+    ));
+    CurrencyIdentifier::from(blake2_256(&(loc, bs).encode()))
+}
+
+#[test]
+fn create_new_shop_works() {
+    ExtBuilder::build().execute_with(|| {
+        // initialisation
+        let cid = register_test_currency();
+        let alice = AccountId::from(AccountKeyring::Alice);        
+        let alice_shop = 40; // for now URL validity is not checked (TODO: IPFS)
+        
+        // upload dummy store to blockchain
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop).is_ok());
+
+        // get shops from blockchain
+        let shops = EncointerBazaar::shop_registry(cid);
+        let alices_shops = EncointerBazaar::shops_owned(cid, alice);
+        // assert that shop was added
+        assert!(shops.contains(&alice_shop));
+        assert!(alices_shops.contains(&alice_shop));
+        // assert that the shop is owned by alice
+        assert_eq!(EncointerBazaar::shop_owner(&cid, &alice_shop), alice);
+    });
+}
+
+#[test]
+fn create_new_shop_with_bad_cid_fails() {
+    ExtBuilder::build().execute_with(|| {
+        // initialisation      
+        let alice = AccountId::from(AccountKeyring::Alice);        
+        let alice_shop = 40;
+        let cid = CurrencyIdentifier::from(blake2_256(&(0, alice).encode())); // fails to register cid
+        
+        // assert that upload fails
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop).is_err());
+
+        // get shops from blockchain
+        let shops = EncointerBazaar::shop_registry(cid);
+        let alices_shops = EncointerBazaar::shops_owned(cid, alice);
+        
+        // assert that shop was not added
+        assert_eq!(shops.contains(&alice_shop), false);
+        assert_eq!(alices_shops.contains(&alice_shop), false);
+    });
+}
+
+#[test]
+fn removal_of_shop_works() {
+    ExtBuilder::build().execute_with(|| {
+        // initialisation
+        let cid = register_test_currency();
+        let alice = AccountId::from(AccountKeyring::Alice);      
+        let alice_shop = 40; // for now URL validity is not checked (TODO: IPFS)
+
+        // upload dummy store to blockchain
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop).is_ok());
+
+        // get shops from blockchain
+        let mut shops = EncointerBazaar::shop_registry(cid);
+        let mut alices_shops = EncointerBazaar::shops_owned(cid, alice);
+        // assert that shop was added
+        assert!(shops.contains(&alice_shop));
+        assert!(alices_shops.contains(&alice_shop));
+        // assert that the shop is owned by alice
+        assert_eq!(EncointerBazaar::shop_owner(&cid, &alice_shop), alice);
+
+        // remove shop from blockchain
+        assert!(EncointerBazaar::remove_shop(Origin::signed(alice.clone()), cid, alice_shop).is_ok());
+
+        // update local shop list
+        shops = EncointerBazaar::shop_registry(cid);
+        alices_shops = EncointerBazaar::shops_owned(cid, alice);
+
+        // assert that shop was removed
+        assert_eq!(shops.contains(&alice_shop), false);
+        assert_eq!(alices_shops.contains(&alice_shop), false);
+        // TODO: How to assert that hash key is exisiting or empty?
+        //assert_eq!(EncointerBazaar::shop_owner(&cid, &alice_shop), None);
+
+    });
+}
+
+#[test]
+fn alices_store_are_differentiated() {
+    ExtBuilder::build().execute_with(|| {
+        // initialisation
+        let cid = register_test_currency();
+        let alice = AccountId::from(AccountKeyring::Alice);
+        let alice_shop_one = 40; 
+        let alice_shop_two = 50;
+
+        // upload stores to blockchain
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop_one).is_ok());
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop_two).is_ok());
+
+        // get shops from blockchain
+        let mut shops = EncointerBazaar::shop_registry(cid);
+        let mut alices_shops = EncointerBazaar::shops_owned(cid, alice);
+        // assert that shops were added
+        assert!(shops.contains(&alice_shop_one));
+        assert!(shops.contains(&alice_shop_two));
+
+        assert!(alices_shops.contains(&alice_shop_one));
+        assert!(alices_shops.contains(&alice_shop_two));
+
+        // assert that the shops are owned by alice
+        assert_eq!(EncointerBazaar::shop_owner(&cid, &alice_shop_one), alice);
+        assert_eq!(EncointerBazaar::shop_owner(&cid, &alice_shop_two), alice);
+
+        // delete shop two
+        assert!(EncointerBazaar::remove_shop(Origin::signed(alice.clone()), cid, alice_shop_two).is_ok());   
+        
+        // assert that shop two was removed and shop one still exisits
+        shops = EncointerBazaar::shop_registry(cid);
+        alices_shops = EncointerBazaar::shops_owned(cid, alice);
+
+        assert!(shops.contains(&alice_shop_one));        
+        assert_eq!(shops.contains(&alice_shop_two), false);
+
+        assert!(alices_shops.contains(&alice_shop_one));
+        assert_eq!(alices_shops.contains(&alice_shop_two), false);       
+    });
+}
+
+#[test]
+fn stores_cannot_be_uploaded_twice() {
+    ExtBuilder::build().execute_with(|| {
+        // initialisation
+        let cid = register_test_currency();
+        let alice = AccountId::from(AccountKeyring::Alice);
+        let alice_shop_one = 40; 
+        let alice_shop_two = 40;
+
+        // upload stores to blockchain
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop_one).is_ok());
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop_two).is_err());
+
+        // get shops from blockchain
+        let shops = EncointerBazaar::shop_registry(cid);
+        let alices_shops = EncointerBazaar::shops_owned(cid, alice);
+
+        // Assert that the shop has been uploaded correctly
+        assert!(shops.contains(&alice_shop_one));   
+        assert!(alices_shops.contains(&alice_shop_one)); 
+    });
+}
+
+#[test]
+fn bob_cannot_remove_alices_store() {
+    ExtBuilder::build().execute_with(|| {
+        // initialisation
+        let cid = register_test_currency();
+        let alice = AccountId::from(AccountKeyring::Alice);
+        let bob = AccountId::from(AccountKeyring::Bob);
+        let alice_shop = 40; 
+        let bob_shop = 50;
+
+        // upload stores to blockchain
+        assert!(EncointerBazaar::upload_shop(Origin::signed(alice.clone()), cid, alice_shop).is_ok());
+        assert!(EncointerBazaar::upload_shop(Origin::signed(bob.clone()), cid, bob_shop).is_ok());
+
+        // get shops from blockchain
+        let mut shops = EncointerBazaar::shop_registry(cid);
+        let mut alices_shops = EncointerBazaar::shops_owned(cid, alice);
+        let bobs_shops = EncointerBazaar::shops_owned(cid, bob);
+        // assert that shops were added
+        assert!(shops.contains(&alice_shop));
+        assert!(shops.contains(&bob_shop));
+
+        assert!(alices_shops.contains(&alice_shop));
+        assert!(bobs_shops.contains(&bob_shop));
+
+        // assert that the shops are owned by alice or bob respective
+        assert_eq!(EncointerBazaar::shop_owner(&cid, &alice_shop), alice);
+        assert_eq!(EncointerBazaar::shop_owner(&cid, &bob_shop), bob);
+
+        // assert that bob can not delete alices shop
+        assert!(EncointerBazaar::remove_shop(Origin::signed(bob.clone()), cid, alice_shop).is_err()); 
+
+        // assert that shop has not been deleted
+        alices_shops = EncointerBazaar::shops_owned(cid, alice);
+        shops = EncointerBazaar::shop_registry(cid);
+
+        assert!(alices_shops.contains(&alice_shop));
+        assert!(shops.contains(&alice_shop));
+    });
+}
