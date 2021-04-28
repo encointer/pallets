d43bd73e58f9ea54988f27d1c2e625045d400416
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-04-28 11:23:44 +0200

    [communities/rpc] add error handling

--- communities/rpc/src/lib.rs
+++ communities/rpc/src/lib.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 
 use encointer_communities_rpc_runtime_api::CommunitiesApi as CommunitiesRuntimeApi;
 use encointer_primitives::common::PalletString;
+use encointer_primitives::communities::CommunityIdentifier;
 
 #[rpc]
 pub trait CommunitiesApi<BlockHash> {
@@ -39,16 +40,35 @@ where
         let api = self.client.runtime_api();
         let at = BlockId::hash(at.unwrap_or_else(|| self.client.info().best_hash));
 
-        let cids = api.get_cids(&at);
+        let cids = api.get_cids(&at)
+            .map_err(runtime_error_into_rpc_err)?;
 
         println!("Cids {:?}", cids);
-
         let mut names :Vec<PalletString> = vec![];
 
-        for cid in cids.unwrap().iter() {
-            names.push(api.get_name(&at, cid).unwrap().unwrap())
+        for cid in cids.iter() {
+            api.get_name(&at, cid)
+                .map_err(runtime_error_into_rpc_err)?
+                .map_or_else(|| warn_storage_inconsistency(cid) ,|name| names.push(name));
         }
 
         Ok(names)
     }
+}
+
+/// Arbitrary number, but substrate uses the same
+const RUNTIME_ERROR: i64 = 1;
+
+/// Converts a runtime trap into an RPC error.
+fn runtime_error_into_rpc_err(err: impl std::fmt::Debug) -> Error {
+    Error {
+        code: ErrorCode::ServerError(RUNTIME_ERROR),
+        message: "Runtime trapped".into(),
+        data: Some(format!("{:?}", err).into()),
+    }
+}
+
+/// This should never happen!
+fn warn_storage_inconsistency(cid: &CommunityIdentifier) {
+    log::warn!("Storage inconsistency. Could not find cid: {:?} in runtime storage. This is a fatal bug in the pallet", cid)
 }
