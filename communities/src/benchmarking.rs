0d16f54cf839fca268b0d4053bc2dff48745bafb
pifragile <pigu@bluewin.ch>
2021-07-09 09:46:33 +0200

    implement benchmarking for new_community

--- communities/src/benchmarking.rs
+++ communities/src/benchmarking.rs
@@ -1,23 +1,37 @@
 use crate::{*, Module as PalletModule};
-use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite};
+use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite, account};
 use frame_system::RawOrigin;
+use encointer_primitives::{
+    communities::{Location, CommunityMetadata, NominalIncome},
+    balances:: {Demurrage, consts::DEFAULT_DEMURRAGE}
+};
+
+const NUM_LOCATIONS : u32 =  1000;
+const NUM_BOOTSTRAPPERS : u32 = 12;
 
 benchmarks! {
-   // new_community {
-    //     let b in 1 .. 1_000_000;
-    //     // The caller account is whitelisted for DB reads/write by the benchmarking macro.
-    //     let caller: T::AccountId = whitelisted_caller();
-    // }: _(RawOrigin::Signed(caller), b.into())
-    sort_vector {
-    let x in 0 .. 10000;
-    let mut m = Vec::<u32>::new();
-    for i in (0..x).rev() {
-        m.push(i);
-    }
-	}: {
-		// The benchmark execution phase could also be a closure with custom code
-		m.sort();
-	}
+    new_community {
+    let i in 1 .. NUM_LOCATIONS;
+    let j in 3 .. NUM_BOOTSTRAPPERS;
+
+    let caller: T::AccountId = whitelisted_caller();
+
+    // spread locations along the equator, ie. lat = 0, lon equally spread over [-170, 170]
+    // using 170 to have a save distance from the dateline
+    let locations: Vec<Location> = (0..NUM_LOCATIONS)
+    .map(|x| Location{lat: Degree::from_num(0.0), lon: Degree::from_num(((x as f64 * 340.0) / (NUM_LOCATIONS as f64)) - 170.0)})
+    .collect();
+
+    let bootrappers : Vec<T::AccountId> = (0..NUM_BOOTSTRAPPERS).map(|n| account("dummy name", n, n)).collect();
+
+    let mut community_metadata = CommunityMetadata::default();
+    community_metadata.name = "20charsaaaaaaaaaaaaa".into();
+    community_metadata.url = Some("19charsaaaaaaaaa.ch".into());
+
+    let demurrage = Some(Demurrage::from_num(DEFAULT_DEMURRAGE));
+    let nominal_income = Some(NominalIncome::from_num(1));
+
+	}: _(RawOrigin::Signed(caller), (&locations[..(i as usize)]).to_vec(), (&bootrappers[..(j as usize)]).to_vec(), community_metadata, demurrage, nominal_income)
 }
 
 
