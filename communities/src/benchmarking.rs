260551e2342d54a75e0fd93905b7f6326d49efdb
pifragile <pigu@bluewin.ch>
2021-07-09 11:30:56 +0200

    fix distance to dateline

--- communities/src/benchmarking.rs
+++ communities/src/benchmarking.rs
@@ -16,10 +16,10 @@ benchmarks! {
 
     let caller: T::AccountId = whitelisted_caller();
 
-    // spread locations along the equator, ie. lat = 0, lon equally spread over [-170, 170]
-    // using 170 to have a save distance from the dateline
+    // spread locations along the equator, ie. lat = 0, lon equally spread over [-150, 150]
+    // using 150 to have a save distance from the dateline
     let locations: Vec<Location> = (0..NUM_LOCATIONS)
-    .map(|x| Location{lat: Degree::from_num(0.0), lon: Degree::from_num(((x as f64 * 340.0) / (NUM_LOCATIONS as f64)) - 170.0)})
+    .map(|x| Location{lat: Degree::from_num(0.0), lon: Degree::from_num(((x as f64 * 300.0) / (NUM_LOCATIONS as f64)) - 150.0)})
     .collect();
 
     let bootrappers : Vec<T::AccountId> = (0..NUM_BOOTSTRAPPERS).map(|n| account("dummy name", n, n)).collect();
