74831af8bfa8234cf13bbb2440297390f2cb1f81
pifragile <pigu@bluewin.ch>
2021-07-23 11:37:09 +0200

    implement benchmarking for new algorithm

--- communities/src/benchmarking.rs
+++ communities/src/benchmarking.rs
@@ -1,12 +1,13 @@
 use crate::{*, Module as PalletModule};
 use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite, account};
 use frame_system::RawOrigin;
+use frame_system::Origin;
 use encointer_primitives::{
     communities::{Location, CommunityMetadata, NominalIncome},
     balances:: {Demurrage, consts::DEFAULT_DEMURRAGE}
 };
 
-const NUM_LOCATIONS : u32 =  2000;
+const NUM_LOCATIONS : u32 =  20000;
 
 benchmarks! {
     new_community {
@@ -15,20 +16,27 @@ benchmarks! {
 
     // spread locations along the equator, ie. lat = 0, lon equally spread over [-150, 150]
     // using 150 to have a save distance from the dateline
-    let locations: Vec<Location> = (0..NUM_LOCATIONS)
+    let locations: Vec<Location> = (0..i + 1)
     .map(|x| Location{lat: Degree::from_num(0.0), lon: Degree::from_num(((x as f64 * 300.0) / (NUM_LOCATIONS as f64)) - 150.0)})
     .collect();
 
-    let bootrappers : Vec<T::AccountId> = (0..12).map(|n| account("dummy name", n, n)).collect();
-
+    let bootstrappers : Vec<T::AccountId> = (0..12).map(|n| account("dummy name", n, n)).collect();
     let mut community_metadata = CommunityMetadata::default();
     community_metadata.name = "20charsaaaaaaaaaaaaa".into();
     community_metadata.url = Some("19charsaaaaaaaaa.ch".into());
-
     let demurrage = Some(Demurrage::from_num(DEFAULT_DEMURRAGE));
     let nominal_income = Some(NominalIncome::from_num(1));
 
-	}: _(RawOrigin::Signed(caller), (&locations[..(i as usize)]).to_vec(), bootrappers, community_metadata, demurrage, nominal_income)
+    // setup test community
+    PalletModule::<T>::new_community(RawOrigin::Signed(caller.clone()).into(), locations[0usize], bootstrappers.clone(), community_metadata.clone(), demurrage.clone(), nominal_income.clone());
+    let cid = CommunityIdentifier::from(blake2_256(&(locations[0usize].clone(), bootstrappers.clone()).encode()));
+    for j in 1..i {
+        PalletModule::<T>::add_location(RawOrigin::Root.into(), cid, locations[j as usize]);
+    }
+
+
+
+	}: _(RawOrigin::Signed(caller), locations[i as usize], bootstrappers, community_metadata, demurrage, nominal_income)
 }
 
 
