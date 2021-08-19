7e7a06cc21bb585d597f5d0f8cf5b1275003f3b4
pifragile <pigu@bluewin.ch>
2021-08-19 13:13:50 +0200

    Feat/benchmarking setup for communities pallet (#52)
    * setup benchmarking for communities pallet
    
    * implement benchmarking for new_community
    
    * fix distance to dateline
    
    * hardcode bootstrappers

---
+++ communities/src/benchmarking.rs
@@ -0,0 +1,35 @@
+use crate::{*, Module as PalletModule};
+use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite, account};
+use frame_system::RawOrigin;
+use encointer_primitives::{
+    communities::{Location, CommunityMetadata, NominalIncome},
+    balances:: {Demurrage, consts::DEFAULT_DEMURRAGE}
+};
+
+const NUM_LOCATIONS : u32 =  2000;
+
+benchmarks! {
+    new_community {
+    let i in 1 .. NUM_LOCATIONS;
+    let caller: T::AccountId = whitelisted_caller();
+
+    // spread locations along the equator, ie. lat = 0, lon equally spread over [-150, 150]
+    // using 150 to have a save distance from the dateline
+    let locations: Vec<Location> = (0..NUM_LOCATIONS)
+    .map(|x| Location{lat: Degree::from_num(0.0), lon: Degree::from_num(((x as f64 * 300.0) / (NUM_LOCATIONS as f64)) - 150.0)})
+    .collect();
+
+    let bootrappers : Vec<T::AccountId> = (0..12).map(|n| account("dummy name", n, n)).collect();
+
+    let mut community_metadata = CommunityMetadata::default();
+    community_metadata.name = "20charsaaaaaaaaaaaaa".into();
+    community_metadata.url = Some("19charsaaaaaaaaa.ch".into());
+
+    let demurrage = Some(Demurrage::from_num(DEFAULT_DEMURRAGE));
+    let nominal_income = Some(NominalIncome::from_num(1));
+
+	}: _(RawOrigin::Signed(caller), (&locations[..(i as usize)]).to_vec(), bootrappers, community_metadata, demurrage, nominal_income)
+}
+
+
+//impl_benchmark_test_suite!(PalletModule, crate::tests::new_test_ext(), crate::tests::Test);
