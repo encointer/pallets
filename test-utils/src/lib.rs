d2032b96a73c4b4f0861e883442224c924914e7a
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-02-09 20:09:15 +0100

    [test-utils] implement CouncilOrigin for communities

--- test-utils/src/lib.rs
+++ test-utils/src/lib.rs
@@ -190,6 +190,7 @@ macro_rules! impl_encointer_communities {
 	($t:ident) => {
 		impl encointer_communities::Config for $t {
 			type Event = Event;
+			type CouncilOrigin = EnsureAlice;
 			type MinSolarTripTimeS = MinSolarTripTimeS;
 			type MaxSpeedMps = MaxSpeedMps;
 		}
