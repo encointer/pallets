6ab121fe40cc7542faf3c1442c8e3f7f812b0f57
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-03-19 07:26:38 +0100

    [ceremonies/benchmarks] helper code compiles with runtime

--- ceremonies/src/benchmarking.rs
+++ ceremonies/src/benchmarking.rs
@@ -8,15 +8,30 @@ use frame_support::{
 	traits::{OnFinalize, OnInitialize},
 };
 use frame_system::RawOrigin;
-use sp_application_crypto::{sp_core, sr25519, Pair};
-use sp_runtime::traits::UniqueSaturatedInto;
+use sp_application_crypto::KeyTypeId;
+use sp_core::{crypto::ByteArray, sr25519};
+use sp_runtime::{traits::UniqueSaturatedInto, RuntimeAppPublic};
 
 pub const GENESIS_TIME: u64 = 1_585_058_843_000;
 pub const ONE_DAY: u64 = 86_400_000;
 pub const BLOCKTIME: u64 = 3_600_000;
 
-fn pair(seed: &[u8; 32]) -> sr25519::Pair {
-	sr25519::Pair::from_seed(seed)
+pub const TEST_KEY_TYPE_ID: KeyTypeId = KeyTypeId(*b"test");
+
+mod app_sr25519 {
+	use super::TEST_KEY_TYPE_ID;
+	use sp_application_crypto::{app_crypto, sr25519};
+	app_crypto!(sr25519, TEST_KEY_TYPE_ID);
+}
+
+type TestPublic = app_sr25519::Public;
+
+fn pair(seed: &[u8; 32]) -> TestPublic {
+	TestPublic::generate_pair(Some(seed.to_vec()))
+}
+
+fn sign(signer: &TestPublic, data: &Vec<u8>) -> sr25519::Signature {
+	signer.sign(data).unwrap().into()
 }
 
 fn create_community<T: Config>() -> CommunityIdentifier {
@@ -64,22 +79,15 @@ where
 	T::Moment::from(time as u64)
 }
 
-pub fn account_id<T: Config>(pair: &sr25519::Pair) -> T::AccountId
-where
-	<T as frame_system::Config>::AccountId: From<sp_core::sr25519::Public>,
-{
-	pair.public().into()
-}
-
 fn create_proof_of_attendance<T: Config>(
 	prover: T::AccountId,
 	cid: CommunityIdentifier,
 	cindex: CeremonyIndexType,
-	attendee: &sr25519::Pair,
+	attendee: &TestPublic,
 ) -> ProofOfAttendance<T::Signature, T::AccountId>
 where
-	<T as frame_system::Config>::AccountId: From<sp_core::sr25519::Public>,
-	<T as Config>::Signature: From<sp_core::sr25519::Signature>,
+	<T as frame_system::Config>::AccountId: ByteArray,
+	<T as Config>::Signature: From<sr25519::Signature>,
 {
 	let msg = (prover.clone(), cindex);
 	ProofOfAttendance {
@@ -87,12 +95,12 @@ where
 		community_identifier: cid,
 		ceremony_index: cindex,
 		attendee_public: account_id::<T>(&attendee),
-		attendee_signature: T::Signature::from(attendee.sign(&msg.encode())),
+		attendee_signature: T::Signature::from(sign(attendee, &msg.encode())),
 	}
 }
 
 fn get_all_claims<T: Config>(
-	attestees: &Vec<sr25519::Pair>,
+	attestees: Vec<TestPublic>,
 	cid: CommunityIdentifier,
 	cindex: CeremonyIndexType,
 	mindex: MeetupIndexType,
@@ -101,23 +109,24 @@ fn get_all_claims<T: Config>(
 	n_participants: u32,
 ) -> Vec<ClaimOfAttendance<T::Signature, T::AccountId, T::Moment>>
 where
-	<T as frame_system::Config>::AccountId: From<sp_core::sr25519::Public>,
-	<T as Config>::Signature: From<sp_core::sr25519::Signature>,
+	<T as frame_system::Config>::AccountId: ByteArray,
+	<T as Config>::Signature: From<sr25519::Signature>,
 {
 	let mut claims: Vec<ClaimOfAttendance<T::Signature, T::AccountId, T::Moment>> = vec![];
-	for a in attestees {
-		claims.push(
-			ClaimOfAttendance::<T::Signature, T::AccountId, T::Moment>::new_unsigned(
-				a.public().into(),
-				cindex,
-				cid,
-				mindex,
-				location,
-				timestamp,
-				n_participants,
-			)
-			.sign(a),
+	for a in attestees.into_iter() {
+		let mut claim = ClaimOfAttendance::<T::Signature, T::AccountId, T::Moment>::new_unsigned(
+			account_id::<T>(&a),
+			cindex,
+			cid,
+			mindex,
+			location,
+			timestamp,
+			n_participants,
 		);
+
+		claim.claimant_signature = Some(sign(&a, &claim.payload_encoded()).into());
+
+		claims.push(claim);
 	}
 	claims
 }
@@ -164,33 +173,38 @@ pub fn set_timestamp<T: Config>(t: T::Moment) {
 	let _ = pallet_timestamp::Pallet::<T>::set(RawOrigin::None.into(), t);
 }
 
+pub fn account_id<T: Config>(account: &TestPublic) -> T::AccountId
+where
+	<T as frame_system::Config>::AccountId: ByteArray,
+{
+	T::AccountId::from_slice(account.as_slice()).unwrap()
+}
+
 fn fake_last_attendance_and_get_proof<T: Config>(
-	prover: &sr25519::Pair,
+	prover: &TestPublic,
 	cid: CommunityIdentifier,
 ) -> ProofOfAttendance<T::Signature, T::AccountId>
 where
-	<T as frame_system::Config>::AccountId: From<sp_core::sr25519::Public>,
-	<T as Config>::Signature: From<sp_core::sr25519::Signature>,
+	<T as frame_system::Config>::AccountId: ByteArray,
+	<T as Config>::Signature: From<sr25519::Signature>,
 {
 	let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
-	let prover_account_id = account_id::<T>(&prover);
+	let prover_account: T::AccountId = account_id::<T>(prover);
+
 	assert_ok!(encointer_balances::Pallet::<T>::issue(
 		cid,
-		&prover_account_id,
+		&prover_account,
 		NominalIncome::from_num(1)
 	));
-	Pallet::<T>::fake_reputation(
-		(cid, cindex - 1),
-		&account_id::<T>(&prover),
-		Reputation::VerifiedUnlinked,
-	);
+	Pallet::<T>::fake_reputation((cid, cindex - 1), &prover_account, Reputation::VerifiedUnlinked);
 	assert_eq!(
-		Pallet::<T>::participant_reputation((cid, cindex - 1), &prover_account_id),
+		Pallet::<T>::participant_reputation((cid, cindex - 1), &prover_account),
 		Reputation::VerifiedUnlinked
 	);
+
 	let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
 	IssuedRewards::<T>::insert((cid, cindex - 1), 0, ());
-	let proof = create_proof_of_attendance::<T>(prover_account_id, cid, cindex - 1, &prover);
+	let proof = create_proof_of_attendance::<T>(prover_account, cid, cindex - 1, prover);
 	proof
 }
 
@@ -207,12 +221,12 @@ fn register_users<T: Config>(
 	cid: CommunityIdentifier,
 	num_newbies: u32,
 	num_reputables: u32,
-) -> Vec<sr25519::Pair>
+) -> Vec<TestPublic>
 where
-	<T as frame_system::Config>::AccountId: From<sp_core::sr25519::Public>,
-	<T as Config>::Signature: From<sp_core::sr25519::Signature>,
+	<T as frame_system::Config>::AccountId: ByteArray,
+	<T as Config>::Signature: From<sr25519::Signature>,
 {
-	let mut users: Vec<sr25519::Pair> = vec![];
+	let mut users: Vec<TestPublic> = vec![];
 	let mut proofs: Vec<ProofOfAttendance<T::Signature, T::AccountId>> = vec![];
 
 	let num_users_total = num_newbies + num_reputables;
@@ -244,8 +258,8 @@ where
 benchmarks! {
 	where_clause {
 		where
-		<T as frame_system::Config>::AccountId: From<sp_core::sr25519::Public>,
-		<T as Config>::Signature: From<sp_core::sr25519::Signature>,
+		<T as frame_system::Config>::AccountId: ByteArray,
+		<T as Config>::Signature: From<sr25519::Signature>,
 		<T as pallet_timestamp::Config>::Moment: From<u64>,
 		<T as frame_system::Config>::BlockNumber: From<u32>,
 		<T as frame_system::Config>::Event: From<pallet::Event<T>>
@@ -255,15 +269,16 @@ benchmarks! {
 		let cid = create_community::<T>();
 
 		let zoran = pair(&[9u8; 32]);
+		let zoran_account= account_id::<T>(&zoran);
 		let proof = fake_last_attendance_and_get_proof::<T>(&zoran, cid);
 		let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
 
 		assert_eq!(ReputableCount::<T>::get((cid, cindex)), 0);
-	}: _(RawOrigin::Signed(account_id::<T>(&zoran)), cid, Some(proof))
+	}: _(RawOrigin::Signed(zoran_account.clone()), cid, Some(proof))
 	verify {
 		assert_eq!(ReputableCount::<T>::get((cid, cindex)), 1);
 		assert_eq!(
-			Pallet::<T>::participant_reputation((cid, cindex - 1), account_id::<T>(&zoran)),
+			Pallet::<T>::participant_reputation((cid, cindex - 1), zoran_account),
 			Reputation::VerifiedLinked
 		);
 	}
@@ -308,79 +323,94 @@ benchmarks! {
 		assert_ok!(Pallet::<T>::register_participant(RawOrigin::Signed(account_id::<T>(&newbie.clone())).into(), cid, None));
 		let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
 
-		assert_eq!(<EndorseesCount<T>>::get((cid, cindex)), 0);
-	}: _(RawOrigin::Signed(alice), cid, account_id::<T>(&newbie))
-	verify {
-		assert_eq!(<EndorseesCount<T>>::get((cid, cindex)), 1);
-	}
-
-	claim_rewards {
-		frame_system::Pallet::<T>::set_block_number(frame_system::Pallet::<T>::block_number() + 1u32.into()); // this is needed to assert events
-		let cid = create_community::<T>();
-
-
-		let users = register_users::<T>(cid, 2, 8);
-
-		run_to_next_phase::<T>();
-		run_to_next_phase::<T>();
-
-
-		let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
-		let loc = Location { lat: Degree::from_num(1i32), lon: Degree::from_num(1i32) };
-		let time = correct_meetup_time::<T>(cindex, loc);
-		let mindex = 1;
-
-		// attest_claims
-		for i in 0..10 {
-			let attestor = users[i as usize].clone();
-			let mut attestees = users.clone();
-			attestees.remove(i as usize);
-			let claims = get_all_claims::<T>(&attestees, cid, cindex, mindex, loc, time, 10);
-			assert_ok!(Pallet::<T>::attest_claims(RawOrigin::Signed(account_id::<T>(&attestor)).into(), claims));
-		}
-
-		run_to_next_phase::<T>();
-		assert!(!IssuedRewards::<T>::contains_key((cid, cindex), mindex));
-	}: _(RawOrigin::Signed(account_id::<T>(&users[0])), cid)
-	verify {
-		assert_eq!(last_event::<T>(), Some(Event::RewardsIssued(cid, 1, 10).into()));
-		assert!(IssuedRewards::<T>::contains_key((cid, cindex), mindex));
-	}
-
-	set_inactivity_timeout {
-	}: _(RawOrigin::Root, 13)
-	verify {
-		assert_eq!(InactivityTimeout::<T>::get(), 13)
-	}
-
-	set_meetup_time_offset {
-	}: _(RawOrigin::Root, 12i32)
-	verify {
-		assert_eq!(MeetupTimeOffset::<T>::get(), 12i32)
-	}
-
-	set_reputation_lifetime {
-	}: _(RawOrigin::Root, 11)
-	verify {
-		assert_eq!(ReputationLifetime::<T>::get(), 11)
-	}
-
-	set_endorsement_tickets_per_bootstrapper {
-	}: _(RawOrigin::Root, 10)
-	verify {
-		assert_eq!(EndorsementTicketsPerBootstrapper::<T>::get(), 10)
-	}
-
-	purge_community_ceremony {
-		let cid = create_community::<T>();
-		let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
-		let user = pair(&[10u8; 32]);
-		assert_ok!(Pallet::<T>::register_participant(RawOrigin::Signed(account_id::<T>(&user.clone())).into(), cid, Some(fake_last_attendance_and_get_proof::<T>(&user.clone(), cid))));
-		assert_eq!(ReputableCount::<T>::get((cid, cindex)), 1);
-	}: _(RawOrigin::Root, (cid, cindex))
-	verify {
-		assert_eq!(ReputableCount::<T>::get((cid, cindex)), 0);
-	}
+	// endorse_newcomer {
+	// 	let cid = create_community::<T>();
+	// 	let alice: T::AccountId = account("alice", 1, 1);
+	//
+	// 	// issue some income such that nebies are allowed to register
+	// 	assert_ok!(encointer_balances::Pallet::<T>::issue(
+	// 		cid,
+	// 		&alice,
+	// 		NominalIncome::from_num(1)
+	// 	));
+	//
+	// 	let newbie = pair(&[10u8; 32]);
+	// 	assert_ok!(Pallet::<T>::register_participant(RawOrigin::Signed(account_id::<T>(&newbie.clone())).into(), cid, None));
+	// 	let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
+	//
+	// 	assert_eq!(<EndorseesCount<T>>::get((cid, cindex)), 0);
+	// }: _(RawOrigin::Signed(alice), cid, account_id::<T>(&newbie))
+	// verify {
+	// 	assert_eq!(<EndorseesCount<T>>::get((cid, cindex)), 1);
+	// }
+	//
+	// claim_rewards {
+	// 	frame_system::Pallet::<T>::set_block_number(frame_system::Pallet::<T>::block_number() + 1u32.into()); // this is needed to assert events
+	// 	let cid = create_community::<T>();
+	//
+	//
+	// 	let users = register_users::<T>(cid, 2, 8);
+	//
+	// 	run_to_next_phase::<T>();
+	// 	run_to_next_phase::<T>();
+	//
+	//
+	// 	let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
+	// 	let loc = Location { lat: Degree::from_num(1i32), lon: Degree::from_num(1i32) };
+	// 	let time = correct_meetup_time::<T>(cindex, loc);
+	// 	let mindex = 1;
+	//
+	// 	// attest_claims
+	// 	for i in 0..10 {
+	// 		let attestor = users[i as usize].clone();
+	// 		let mut attestees = users.clone();
+	// 		attestees.remove(i as usize);
+	// 		let claims = get_all_claims::<T>(&attestees, cid, cindex, mindex, loc, time, 10);
+	// 		assert_ok!(Pallet::<T>::attest_claims(RawOrigin::Signed(&attestor).into(), claims));
+	// 	}
+	//
+	// 	run_to_next_phase::<T>();
+	// 	assert!(!IssuedRewards::<T>::contains_key((cid, cindex), mindex));
+	// }: _(RawOrigin::Signed(account_id::<T>(&users[0])), cid)
+	// verify {
+	// 	assert_eq!(last_event::<T>(), Some(Event::RewardsIssued(cid, 1, 10).into()));
+	// 	assert!(IssuedRewards::<T>::contains_key((cid, cindex), mindex));
+	// }
+	//
+	// set_inactivity_timeout {
+	// }: _(RawOrigin::Root, 13)
+	// verify {
+	// 	assert_eq!(InactivityTimeout::<T>::get(), 13)
+	// }
+	//
+	// set_meetup_time_offset {
+	// }: _(RawOrigin::Root, 12i32)
+	// verify {
+	// 	assert_eq!(MeetupTimeOffset::<T>::get(), 12i32)
+	// }
+	//
+	// set_reputation_lifetime {
+	// }: _(RawOrigin::Root, 11)
+	// verify {
+	// 	assert_eq!(ReputationLifetime::<T>::get(), 11)
+	// }
+	//
+	// set_endorsement_tickets_per_bootstrapper {
+	// }: _(RawOrigin::Root, 10)
+	// verify {
+	// 	assert_eq!(EndorsementTicketsPerBootstrapper::<T>::get(), 10)
+	// }
+	//
+	// purge_community_ceremony {
+	// 	let cid = create_community::<T>();
+	// 	let cindex = encointer_scheduler::Pallet::<T>::current_ceremony_index();
+	// 	let user = pair(&[10u8; 32]);
+	// 	assert_ok!(Pallet::<T>::register_participant(RawOrigin::Signed(account_id::<T>(&user.clone())).into(), cid, Some(fake_last_attendance_and_get_proof::<T>(&user.clone(), cid))));
+	// 	assert_eq!(ReputableCount::<T>::get((cid, cindex)), 1);
+	// }: _(RawOrigin::Root, (cid, cindex))
+	// verify {
+	// 	assert_eq!(ReputableCount::<T>::get((cid, cindex)), 0);
+	// }
 
 }
 
