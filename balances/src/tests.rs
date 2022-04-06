a71b84a5d50a41f03d1105de2912aad1f857c9a3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-04-06 18:19:23 -0400

    [balances] test that transfer fails with `NoAccount` error if account does not exist in community

--- balances/src/tests.rs
+++ balances/src/tests.rs
@@ -177,6 +177,26 @@ fn transfer_does_not_create_new_account_if_below_ed() {
 }
 
 #[test]
+fn if_account_does_not_exist_in_community_transfer_errs_with_no_account_error() {
+	new_test_ext().execute_with(|| {
+		System::set_block_number(System::block_number() + 1);
+		System::on_initialize(System::block_number());
+
+		let alice = AccountKeyring::Alice.to_account_id();
+
+		// does not exist on chain
+		let zoltan: AccountId32 = sr25519::Pair::from_entropy(&[9u8; 32], None).0.public().into();
+		let cid = CommunityIdentifier::default();
+		let amount = BalanceType::from_num(0.0000000001);
+
+		assert_noop!(
+			EncointerBalances::transfer(Some(alice.clone()).into(), zoltan.clone(), cid, amount),
+			Error::<TestRuntime>::NoAccount,
+		);
+	});
+}
+
+#[test]
 fn demurrage_should_work() {
 	new_test_ext().execute_with(|| {
 		let alice = AccountKeyring::Alice.to_account_id();
