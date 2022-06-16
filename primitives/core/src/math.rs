8af7e4279a645d99c3b64c1d73e1f129d9e0509e
pifragile <pigu@bluewin.ch>
2022-06-16 10:43:27 +0100

    add math.rs

---
+++ primitives/core/src/math.rs
@@ -0,0 +1,24 @@
+use sp_arithmetic::traits::BaseArithmetic;
+pub trait AbsDiff<T> {
+	fn abs_diff(&self, other: T) -> T;
+}
+
+impl<T: BaseArithmetic + Copy> AbsDiff<T> for T {
+	fn abs_diff(&self, other: T) -> T {
+		if *self > other {
+			return *self - other
+		} else {
+			return other - *self
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn abs_diff_works() {
+		assert_eq!(12u64.abs_diff(10u64), 2u64);
+		assert_eq!(10u64.abs_diff(13u64), 3u64);
+		assert_eq!(10u64.abs_diff(10u64), 0u64);
+	}
+}
