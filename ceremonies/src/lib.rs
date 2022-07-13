233da6bcf4684a57677986c24ac483cb95cdca95
pifragile <pigu@bluewin.ch>
2022-07-13 14:37:38 +0200

    flush reputation cache on registering

--- ceremonies/src/lib.rs
+++ ceremonies/src/lib.rs
@@ -168,7 +168,14 @@ pub mod pallet {
 			let participant_type = Self::register(cid, cindex, &sender, proof.is_some())?;
 
 			debug!(target: LOG, "registered participant: {:?} as {:?}", sender, participant_type);
-			Self::deposit_event(Event::ParticipantRegistered(cid, participant_type, sender));
+			Self::deposit_event(Event::ParticipantRegistered(
+				cid,
+				participant_type,
+				sender.clone(),
+			));
+
+			// invalidate reputation cache
+			sp_io::offchain_index::set(&reputation_cache_dirty_key(sender), &true.encode());
 
 			Ok(().into())
 		}
