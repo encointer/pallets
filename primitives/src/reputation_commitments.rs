79ce1e10e6fee566d6c6972b0693359b557e6a06
pifragile <pigu@bluewin.ch>
2023-05-31 15:06:19 +0200

    add sp_std import

--- primitives/src/reputation_commitments.rs
+++ primitives/src/reputation_commitments.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
 use sp_core::{bounded::BoundedVec, ConstU32};
+use sp_std::vec::Vec;
 
 pub type PurposeIdType = u64;
 pub type DescriptorType = BoundedVec<u8, ConstU32<24>>;
