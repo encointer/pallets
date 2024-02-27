7c482b9a08396c0c7cca02fd5335af220089754a
Alain Brenzikofer <alain@integritee.network>
2024-02-27 13:23:11 +0100

    bump storage version

--- ceremonies/src/lib.rs
+++ ceremonies/src/lib.rs
@@ -70,7 +70,7 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
 	#[pallet::pallet]
 	#[pallet::storage_version(STORAGE_VERSION)]
 	#[pallet::without_storage_info]
